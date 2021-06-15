e156e4bd7ac7a9a9bfc621484553eeb087c2569d
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-14 20:03:28 -0400

    Updated server.js to use MongoDB Atlas

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 const app = express();
 
@@ -23,6 +23,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(PORT || 3000, () => {
   console.log(`App running on port ${PORT}!`);
 });

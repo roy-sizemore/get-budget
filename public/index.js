50e2895bcd81d5199e3079dee2a097eb91ded36f
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-17 13:01:36 -0400

    Fixed bug in index .js

--- public/index.js
+++ public/index.js
@@ -2,11 +2,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
       .then((reg) => {
-        console.log('Service worker registered.', reg);
+        return reg
       });
   });
-}
-
+};
 
 let transactions = [];
 let myChart;
